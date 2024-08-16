88d7addb209d3f8dc95629c9c7ccc99b62725b6b
Ubuntu <jenkinsuser@VM1.hhmqxri3uu1u1nnxxqlg5oagjf.fx.internal.cloudapp.net>
2024-08-16 04:52:27 +0000

    commit

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo $BUILD_NO > /tmp/.auth
-echo $PASS >> /tmp/.auth
+echo $BUILD_NUMBER > $(PWD)/.auth
+echo $PASS >> $(PWD)/.auth
 
-scp /tmp/.auth saurabh6422@13.69.148.248:/tmp/.auth
+scp $(PWD)/.auth saurabh6422@13.69.148.248:/tmp/.auth
 scp ./pipeline/deploy1.sh saurabh6422@13.69.148.248:/tmp/target.sh
 ssh saurabh6422@13.69.148.248 "/tmp/target.sh"
